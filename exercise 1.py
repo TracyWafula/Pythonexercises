a00f969a1da83192e6084420d57ac479115b7eff
TracyWafula <tracywafula@gmail.com>
2021-07-23 17:30:22 +0200

    first commit

---
+++ exercise 1.py
@@ -0,0 +1,18 @@
+
+lst = []
+for count in range(6):
+    x = int(input("Enter number"))
+    lst.append(x)
+print("Highest number", max(lst) )
+print("Lowest number", min(lst) )
+
+    
+
+
+
+    
+
+    
+
+
+
