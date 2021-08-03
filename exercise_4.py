48062bc6dcd32353c7236ca9e67241d7357d9351
TracyWafula <tmwafula@usiu.ac.ke>
2021-08-03 08:24:52 +0200

    Add files via upload

---
+++ exercise_4.py
@@ -0,0 +1,13 @@
+#Challenge: Guessing game
+print ("Kindly enter the name of your favourite food")
+fav_food = input("My favourite food is: ")
+
+if not fav_food:
+     print(" Thanks for playing!")
+     print(" Yuck! That's not it!")
+else:
+     print(" Thanks for playing!") 
+     print(" Yep! So amazing!")
+
+
+
---
+++ exercise_5.py
@@ -0,0 +1,17 @@
+#Challenge: Exam grading
+score = int(input("Kindly enter what you exam scored between 0 and up to 100: "))
+if score >= 0  and score<=39:
+    print ("FAIL")
+elif score>=40  and score<=59:
+    print ("PASS")
+elif score>=60  and score<=74:
+    print ("GOOD")
+elif score>=75  and score<=84:
+    print ("VERY GOOD")
+elif score>=85  and score<=100:
+    print ("EXCELLENT")
+elif score>=85  and score<=100:
+    print ("EXCELLENT")
+elif score>=100:
+    print ("ONLY VALUES BETWEEN 0 - 100 ARE ALLOWED")
+    
