69cbfe7a60eb53e6415ddc83f89aa75d2e4c30b6
TracyWafula <tracywafula@gmail.com>
2021-08-08 07:41:49 +0200

    Add files via upload
    collections

---
+++ exercise_6.py
@@ -0,0 +1,17 @@
+#Challenge 1
+stars = ['Sol', 'Alpha Centauri', 'Barnard', 'Wolf 359']
+print(stars[3])
+
+#Challenge 2
+
+peaks = {
+    'African' : 'Kilimanjaro',
+    'Antarctic' : 'Vinson',
+    'Australian' : 'Puncak Jaya',
+    'Eurasian' : 'Everest',
+    'North_America' : 'Denali',
+    'Pacific' : 'Mauna Kea',
+    'South_American' : 'Aconcagua',
+
+}
+print (peaks['Pacific'])
